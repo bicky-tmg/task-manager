7bd1e2b6b4cf55e1fb79cdc6f29ca43d2516226d
Bicky Tamang <bickytamang101@gmail.com>
2026-01-18 16:54:02 +0545

    feat: update task id logic and persist tasks in local storage

--- src/store/taskStore.ts
+++ src/store/taskStore.ts
@@ -1,6 +1,7 @@
 import type { Task } from '@/types/task'
 import type { TaskFormData } from '@/validation/task';
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 type TaskStore = {
     tasks: Task[];
@@ -11,37 +12,41 @@ type TaskStore = {
     deleteTask: (id: string) => void;
 }
 
-export const useTaskStore = create<TaskStore>()((set, get) => ({
-    tasks: [],
-    editingTask: null,
-    setEditingTask: (next) => set((state) => ({ editingTask: typeof next === "function" ? next(state.editingTask) : next })),
-    addTask: (data) => {
-        const now = new Date().toISOString();
-        const newTask: Task = {
-            id: `TM-${get().tasks.length + 1}`,
-            ...data,
-            createdAt: now,
-            updatedAt: now,
-        };
-        set((state) => {
-            const tasks = [...state.tasks, newTask];
-            return { tasks }
-        })
-    },
-    updateTask: (id, data): void => {
-        set((state) => {
-            const tasks = state.tasks.map((task) =>
-                task.id === id
-                    ? { ...task, ...data, updatedAt: new Date().toISOString() }
-                    : task
-            );
-            return { tasks };
-        });
-    },
-    deleteTask: (id: string): void => {
-        set((state) => {
-            const tasks = state.tasks.filter((task) => task.id !== id);
-            return { tasks };
-        });
-    },
-}))
+export const useTaskStore = create<TaskStore>()(
+    persist((set, get) => ({
+        tasks: [],
+        editingTask: null,
+        setEditingTask: (next) => set((state) => ({ editingTask: typeof next === "function" ? next(state.editingTask) : next })),
+        addTask: (data) => {
+            const now = new Date().toISOString();
+            const tasks = get().tasks
+            const lastIdNumber = Number(tasks[tasks.length - 1]?.id?.split("-")?.[1])
+            const newId = isNaN(lastIdNumber) ? "TM-1" : `TM-${lastIdNumber + 1}`
+            const newTask: Task = {
+                id: newId,
+                ...data,
+                createdAt: now,
+                updatedAt: now,
+            };
+            set((state) => {
+                const tasks = [...state.tasks, newTask];
+                return { tasks }
+            })
+        },
+        updateTask: (id, data): void => {
+            set((state) => {
+                const tasks = state.tasks.map((task) =>
+                    task.id === id
+                        ? { ...task, ...data, updatedAt: new Date().toISOString() }
+                        : task
+                );
+                return { tasks };
+            });
+        },
+        deleteTask: (id: string): void => {
+            set((state) => {
+                const tasks = state.tasks.filter((task) => task.id !== id);
+                return { tasks };
+            });
+        },
+    }), { name: 'tasks-storage', partialize: (state) => ({ tasks: state.tasks }) }))
